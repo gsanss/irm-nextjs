184831760eb950682ae1b6cbe0c485fb34e7ad71
Gerardo Sanchez <gerardo@linqto.com>
2025-01-16 23:25:44 -0600

    server side render

--- src/components/LoginSignOut.jsx
+++ src/components/LoginSignOut.jsx
@@ -1,18 +1,27 @@
-import { useSession, useServerSession, signIn, signOut } from "next-auth/react"
-export default function LoginSignOut() {
-  const { data: session } = useSession()
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]"; // Adjust the path if necessary
+import { signIn, signOut } from "next-auth/react";
+
+export default async function LoginSignOut() {
+  const session = await getServerSession(authOptions); // Fetch session data on the server
+
   if (session) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+      <div>
+        Signed in as {session.user?.email} <br />
+        <form action="/api/auth/signout" method="post">
+          <button type="submit">Sign out</button>
+        </form>
+      </div>
+    );
   }
+
   return (
-    <>
+    <div>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+      <form action="/api/auth/signin" method="post">
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
 }
